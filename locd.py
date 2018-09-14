c1880e98318121163fc7e154b83d85407cf7f0d5
AnTi3z <anti3z@anti3z.ru>
2018-09-14 23:55:28 +0300

    Some improvements

--- locd.py
+++ locd.py
@@ -40,12 +40,15 @@ def daemon_start(args):
     logger.info('STARTED')
 
     with open(curf,'r') as f:
+        # TODO: add try/except
         lat, lon = [float(coord) for coord in f.readline().split(',')]
+        logger.info(f'Read from {curf}: Lat: {lat}, Lon: {lon}')
 
-    logger.info(f'Read: Lat: {lat}, Lon: {lon}')
+    # TODO: Create Unix socket for IPC
 
     trck = location.Tracker(lat, lon)
-    trck.move_to(lat+0.01, lon-0.02)
+    if args['cmd'] == 'move':
+        trck.move_to(float(args['lat']), float(args['lat']))
 
     while trck.get_track():
         lat, lon = trck.accurate_loc().pos
@@ -56,16 +59,16 @@ def daemon_start(args):
 
 
 def stopped_daemon(args, pidf):
-    if args.cmd == 'status':
+    if args['cmd'] == 'status':
         print("{'running': false}")
-    elif args.cmd == 'cur':
-        with open(args.cur_file,'r') as f:
+    elif args['cmd'] == 'cur':
+        with open(args['cur_file'],'r') as f:
             print(f.readline())
             #lat, lon = [float(coord) for coord in f.readline().split(',')]
-    elif args.cmd == 'track':
-        print('')
-    elif args.cmd == 'move':
-        context =  daemon.DaemonContext(
+    elif args['cmd'] == 'track':
+        print('[]')
+    elif args['cmd'] in ['move','start']:
+        context = daemon.DaemonContext(
             working_directory='./',
             umask=0o002,
             pidfile=pidf)
@@ -73,28 +76,32 @@ def stopped_daemon(args, pidf):
             daemon_start(args)
 
 
-def running_daemon(args):
+def running_daemon(args, pidf):
     #TODO: IPC connect here
-    if args.cmd == 'status':
+    if args['cmd'] == 'status':
         print('running')
+    elif args['cmd'] == 'move':
+        os.kill(pidf.read_pid(), 15)  # SIGTERM
+        # stop daemon and restart
+        stopped_daemon(args, pidf)
     else:
         print('not impleneted yet')
 
+
 def main(args):
-    #logfile.debug('Invoked main with args: {}'.format(args))
-    pidlockf=pidfile.TimeoutPIDLockFile(args.pid_file)
+    pidlockf = pidfile.TimeoutPIDLockFile(args['pid_file'])
 
     try:
         pidlockf.acquire()
-        #daemon stopped
+        # daemon stopped
         stopped_daemon(args, pidf=pidlockf)
     except AlreadyLocked:
         try:
             os.kill(pidlockf.read_pid(), 0)
-            #daemon running
-            running_daemon(args)
-        except OSError:  #No process with locked PID
-            #daemon been killed (stopped)
+            # daemon running
+            running_daemon(args, pidf=pidlockf)
+        except OSError:  # No process with locked PID
+            # daemon been killed (stopped)
             pidlockf.break_lock()
             stopped_daemon(args, pidf=pidlockf)
 
@@ -104,28 +111,32 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--pid-file', default=PID_FILE, help='(Default: %(default)s)')
     parser.add_argument('-l', '--log-file', default=LOG_FILE, help='(Default: %(default)s)')
     parser.add_argument('-s', '--sock-file', default=SOCK_FILE, help='(Default: %(default)s)')
-    parser.add_argument('-m', '--mmap-file', default=MMAP_FILE, help='(Default: %(default)s)')
+    # parser.add_argument('-m', '--mmap-file', default=MMAP_FILE, help='(Default: %(default)s)')
     parser.add_argument('-c', '--cur-file', default=CUR_LOC_FILE, help='(Default: %(default)s)')
 
     subparsers = parser.add_subparsers(dest='cmd', help='sub-command help')
 
     parser_status = subparsers.add_parser('status', help='Get daemon status')
+    # parser_status.add_argument('-j', '--json', help='')
 
     parser_cur = subparsers.add_parser('cur', help='Get current location')
-    #parser_cur.add_argument('--force-cur', help='')
-    #parser_cur.add_argument('--force-sock', help='')
-    #parser_cur.add_argument('--force-mmap', help='')
+    # parser_cur.add_argument('--force-file', help='')
 
-    parser_move = subparsers.add_parser('move', help='Move to point with given latitude and longitude')
+    parser_start = subparsers.add_parser('start', help='Start daemon and stay it alive')
+    # parser_start.add_argument('--force-file', help='')
+
+    parser_move = subparsers.add_parser('move', help='Move to point with given latitude and longitude'
+                                                     '(daemon will automaticaly start if it not)')
     parser_move.add_argument('lat', type=float, help='Latitude of point')
     parser_move.add_argument('lon', type=float, help='Longitude of point')
-    #parser_move.add_argument('--force-cur', help='')
-    #parser_move.add_argument('--force-sock', help='')
-    #parser_move.add_argument('--force-mmap', help='')
+    # parser_move.add_argument('-a', help='Keep daemon alive')
+    # parser_move.add_argument('--force-file', help='')
 
-    parser_stop = subparsers.add_parser('stop', help='Stop movement and daemon')
+    subparsers.add_parser('stop', help='Stop movement and daemon(if it was started with "move" cmd and'
+                                       ' -a key is ommited)')
 
     parser_track = subparsers.add_parser('track', help='Get current waypoint track')
+    #parser_track.add_argument('-j', '--json', help='')
 
     parser_speed = subparsers.add_parser('speed', help='Setup current movement speed')
     parser_speed.add_argument('spd', type=float, help='Speed in km/h')
